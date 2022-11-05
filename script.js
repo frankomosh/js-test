2685959eadd12a870922e298e70aab0fa06af902
Frank-197 <frankomosh197@gmail.com>
2022-11-05 13:21:52 +0300

    adding a loading animation

--- script.js
+++ script.js
@@ -4,11 +4,24 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader=document.getElementById('loader');
 
 let apiQuotes=[];
 
+//show loading
+function loading(){
+    loader.hidden = false;
+    quoteContainer.hidden=true;
+}
+//hide loading
+function complete(){
+    quoteContainer.hidden=false;
+    loader.hidden=true;
+}
+
 // show new quote
 function newQuote(){
+    loading();
     // pick a random quote from API quotes array
     const quote=apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     
@@ -25,13 +38,16 @@ function newQuote(){
         quoteText.classList.remove('long-quote')
         
     }
+    //set quote, hide loader
 
     quoteText.textContent=quote.text;
+    complete();
 }
 
 // Get quotes from Api
 
 async function getQuotes(){
+    loading();
     const apiUrl='https://type.fit/api/quotes';
     try{
         const response = await fetch(apiUrl);
@@ -52,5 +68,6 @@ newQuoteBtn.addEventListener('click', newQuote);
 twitterBtn.addEventListener('click', tweetQuote);
 // on load
 getQuotes();
+
 
 
